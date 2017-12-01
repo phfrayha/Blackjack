4da82901998ee86a21c51cff909e56373d09a61c
phfrayha <pedro.frayha@gmail.com>
2017-11-30 23:30:35 -0200

    Inclusão do ButtonClickListener no repositório

---
+++ src/java/pedrofrayha/blackjack/actions/DealerButtonClickListener.java
@@ -0,0 +1,30 @@
+package pedrofrayha.blackjack.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import pedrofrayha.blackjack.BlackjackLauncher;
+
+public class DealerButtonClickListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String command = e.getActionCommand();
+		if(command.equalsIgnoreCase("EndGameCommand"))
+		{
+			System.exit(0);
+		}
+		else if(command.equalsIgnoreCase("SaveStateCommand"))
+		{
+			try {
+				BlackjackLauncher.saveState();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+
+}
